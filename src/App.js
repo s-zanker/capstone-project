2e97a53c9ce1348a91b56817f1923b39f3607cb0
s-zanker <grueffelo@gmx.net>
2020-07-07 17:33:20 +0200

    Move components to subfolder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import Header from './components/Header'
+import Header from './components/Header/Header'
+import styled from 'styled-components'
+import WorkspaceList from './components/WorkspaceList/WorkspaceList'
+import workspaces from './assets/workspaces.json'
 
 function App() {
   return (
     <>
       <Header />
-      <h2>Das ist h2</h2>
+      <StyledTitle>Shared Workspaces</StyledTitle>
+      <WorkspaceList workspaces={workspaces} />
     </>
   )
 }
 
 export default App
+
+const StyledTitle = styled.h2`
+  color: var(--darkgreen);
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+`
