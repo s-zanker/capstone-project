44c4f7e981cec453aab681de17f3c3ed6ddd4682
s-zanker <grueffelo@gmx.net>
2020-07-13 18:08:31 +0200

    Clean code comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 import workspaces from './assets/workspaces.json'
@@ -7,45 +7,21 @@ import WorkspaceDetail from './components/WorkspaceDetail/WorkspaceDetail'
 import WorkspaceList from './components/WorkspaceList/WorkspaceList'
 
 function App() {
-  const [workspaceList, setWorkspaceList] = useState(workspaces)
-
-  /*  useEffect(() => {
-    addWorkspace(
-      JSON.stringify({
-        _id: '5f0734c2af7264771ca52659',
-        name: 'Alsenhof',
-        description:
-          'Gemeinsam lassen wir nördlich von Hamburg einen nachhaltigen, innovativen und offenen Lebens- und Arbeitsort im Grünen entstehen!',
-        location: 'Lägerdorf',
-        street: 'Dorfstraße 15',
-        postcode: '25566',
-        state: 'Schleswig-Holstein',
-        url: 'https://www.alsenhof.de',
-        email: 'ulrich@coworkland.de',
-        phone: '+49 40 60942876',
-        picture: '/images/justin-lim-acGAEEMguPo-unsplash.jpg',
-      })
-    )
-  }, []) */
   return (
     <AppGrid>
       <Header title="digitalnatur" />
       <Main>
         <Switch>
           <Route exact path="/">
-            <WorkspaceList workspaces={workspaceList} />
+            <WorkspaceList workspaces={workspaces} />
           </Route>
           <Route path="/workspace/:id">
-            <WorkspaceDetail workspaces={workspaceList} />
+            <WorkspaceDetail workspaces={workspaces} />
           </Route>
         </Switch>
       </Main>
     </AppGrid>
   )
-
-  function addWorkspace(newWorkspace) {
-    setWorkspaceList(...workspaceList, newWorkspace)
-  }
 }
 
 export default App
