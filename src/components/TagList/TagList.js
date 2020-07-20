59b6f8dc43e161c811385401e547bddb2ee911ab
s-zanker <grueffelo@gmx.net>
2020-07-20 16:50:00 +0200

    Add a tag-list component and story to storybook

---
+++ src/components/TagList/TagList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function TagList() {
+  return <TagContainerStyled>TagList</TagContainerStyled>
+}
+
+const TagContainerStyled = styled.div`
+  background: white;
+  box-shadow: 4px 4px 8px #245f49;
+  height: 250px;
+  width: 300px;
+  padding: 20px;
+`
